1e1778567699253f0382a0fe826b2288e57ce283
Subham Sahu <subham.sahu@brand-scapes.com>
2024-12-24 00:44:33 +0530

    changed deployment file

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -20,7 +20,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://victorious-river-09d149f03.4.azurestaticapps.net' 
+    'https://web-app-django-deployment-bkbyb3awaehpctb4.westus-01.azurewebsites.net' 
 ]
 
 
